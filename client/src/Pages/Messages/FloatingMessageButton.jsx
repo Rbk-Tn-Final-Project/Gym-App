10f3234a7a9159163a6181c06632ac72430367c1
Alaya-Sendi <sendialaya@gmail.com>
2024-08-12 18:50:26 +0000

    Fixed message routing issues and updated FloatingMessageButton to use useNavigate

--- client/src/Pages/Messages/FloatingMessageButton.jsx
+++ client/src/Pages/Messages/FloatingMessageButton.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './FloatingMessageButton.css';
 
 const FloatingMessageButton = () => {
-    const history = useHistory();
+    const navigate = useNavigate();
 
     const handleClick = () => {
-        history.push('/compose-message');
+        navigate('/compose-message');
     };
 
     return (
